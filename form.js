c9cb52fc0cb9582f8ecabe00f12ed07b20de66c9
SakuyaYae <89073955+SakuyaYae@users.noreply.github.com>
2023-09-25 11:50:59 +0200

    Updated questions and js functonalety

--- form.js
+++ form.js
@@ -3,31 +3,47 @@ const questions = require("./questions.json");
 const fs = require("fs");
 const userInput = require("prompt-sync")({ sigint: true });
 
-var awnser;
+const questionsArr = [questions[0].question1, questions[0].question2, questions[0].question3, questions[0].question4, questions[0].question5, questions[0].question6, questions[0].question7, questions[0].question8, questions[0].question9, questions[0].question10, questions[0].question11, questions[0].question12, questions[0].question13, questions[0].question14, questions[0].question15];
 const awnsers = {
   "cat": 0,
   "dog": 0,
   "rabbit": 0,
-  "fish": 0
+  "fish": 0,
+  "name": "",
+  "date": ""
 };
+var awnser;
 
-console.log(questions[0].question1.question + "\n Yes \n No");
-awnser = userInput("").trim().toLowerCase();
+console.log("Ange ditt namn: ");
+awnser = userInput("").trim();
 
-if (awnser === "yes") {
-  awnsers.cat += questions[0].question1.yes.pointsCat;
-  awnsers.dog += questions[0].question1.yes.pointsDog;
-  awnsers.rabbit += questions[0].question1.yes.pointsRabbit;
-  awnsers.fish += questions[0].question1.yes.pointsFish;
-}
-else if (awnser === "no") {
-  awnsers.cat += questions[0].question1.no.pointsCat;
-  awnsers.dog += questions[0].question1.no.pointsDog;
-  awnsers.rabbit += questions[0].question1.no.pointsRabbit;
-  awnsers.fish += questions[0].question1.no.pointsFish;
-}
-else {
-  console.log("Wrong awnser, you must awnser yes or no!");
+awnsers.name = awnser;
+awnsers.date = Date();
+
+for (var i = 0; i < questionsArr.length; i++) {
+  console.log(questionsArr[i].question + "\n Ja \n Nej");
+  awnser = userInput("").trim().toLowerCase();
+
+  if (awnser === "yes") {
+    awnsers.cat += questionsArr[i].yes.pointsCat;
+    awnsers.dog += questionsArr[i].yes.pointsDog;
+    awnsers.rabbit += questionsArr[i].yes.pointsRabbit;
+    awnsers.fish += questionsArr[i].yes.pointsFish;
+  }
+  else if (awnser === "no") {
+    awnsers.cat += questionsArr[i].no.pointsCat;
+    awnsers.dog += questionsArr[i].no.pointsDog;
+    awnsers.rabbit += questionsArr[i].no.pointsRabbit;
+    awnsers.fish += questionsArr[i].no.pointsFish;
+  }
+  else {
+    console.log("Du måste svara Ja eller Nej!");
+  }
 }
 
+console.log(awnsers.name);
+console.log(awnsers.date);
 console.log(awnsers.cat);
+console.log(awnsers.dog);
+console.log(awnsers.rabbit);
+console.log(awnsers.fish);
